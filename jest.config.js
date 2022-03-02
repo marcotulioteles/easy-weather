77cc6febe476391807e0876fc7bbbcf89ccba4c5
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2022-03-02 09:34:05 -0300

    test: setup jest and react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules', '/.next"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts"
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+  ],
+  coverageReporters: ["lcov", "json"]
+};
